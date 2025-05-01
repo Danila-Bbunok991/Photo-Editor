73ecdb7089aab8677399b802699a35fd5800e741
Danila-Bbunok991 <danila230304@icloud.com>
2025-05-01 10:27:45 +0300

    Initial Commit

---
+++ Photo_Editor/Photo_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_EditorApp.swift
+//  Photo_Editor
+//
+//  Created by Bbunok991 on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Photo_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photo_EditorTests/Photo_EditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_EditorTests.swift
+//  Photo_EditorTests
+//
+//  Created by Bbunok991 on 5/1/25.
+//
+
+import Testing
+@testable import Photo_Editor
+
+struct Photo_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
